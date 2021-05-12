feb40a11219c9bde5753a435acbbe9ee98bf8533
patrick-angelos <angelosp07@gmail.com>
2021-05-12 14:56:57 +0300

    Create non functional Book creation form

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const BooksForm = () => (
-  <div />
-);
+const BooksForm = () => {
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const options = categories.map((option) => (
+    <option key={option} value={option}>{option}</option>
+  ));
+  return (
+    <form>
+      <label htmlFor="title">
+        Title
+        <input id="title" type="text" />
+      </label>
+      <select>
+        {options}
+      </select>
+      <input type="submit" value="Create" />
+    </form>
+  );
+};
 
 export default BooksForm;
