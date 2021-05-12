02236dc8a9baa6e43c3404589e89a441be5197d9
patrick-angelos <angelosp07@gmail.com>
2021-05-12 14:38:08 +0300

    Add Book Component and connect redux with components

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Book from '../components/Book';
 
-const BooksList = () => (
-  <table>
-    <tr>
-      <th>Book ID</th>
-      <th>Title</th>
-      <th>Category</th>
-    </tr>
-  </table>
-);
+const BooksList = (props) => {
+  const { books } = props;
+  const list = books.map((book) => (
+    <Book key={book.id} book={book} />
+  ));
 
-export default BooksList;
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Book ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+        {list}
+      </tbody>
+    </table>
+  );
+};
+
+BooksList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  books: state.books,
+});
+
+export default connect(mapStateToProps)(BooksList);
--- src/index.js
+++ src/index.js
@@ -9,9 +9,14 @@ import rootReducer from './reducers/index';
 const state = {
   books: [
     {
-      id: Math.random(),
-      title: 'title',
-      category: 'fcrfr',
+      id: Math.round(Math.random() * 1000),
+      title: 'Maths',
+      category: 'Learning',
+    },
+    {
+      id: Math.round(Math.random() * 1000),
+      title: 'The Alchemist',
+      category: 'Novel',
     },
   ],
 };
