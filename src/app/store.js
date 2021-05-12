274d03c7161cda05b0548857a9691143f5bcfcbc
patrick-angelos <angelosp07@gmail.com>
2021-05-12 14:41:20 +0300

    Seperate store from index

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { createStore } from 'redux';
+import rootReducer from '../reducers/index';
 
-export const store = configureStore({
-  reducer: {
-  },
-});
+const state = {
+  books: [
+    {
+      id: Math.round(Math.random() * 1000),
+      title: 'Maths',
+      category: 'Learning',
+    },
+    {
+      id: Math.round(Math.random() * 1000),
+      title: 'The Alchemist',
+      category: 'Novel',
+    },
+  ],
+};
+
+const store = createStore(rootReducer, state);
 
 export default store;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import App from './components/App';
 import './styles/reset.css';
-import rootReducer from './reducers/index';
-
-const state = {
-  books: [
-    {
-      id: Math.round(Math.random() * 1000),
-      title: 'Maths',
-      category: 'Learning',
-    },
-    {
-      id: Math.round(Math.random() * 1000),
-      title: 'The Alchemist',
-      category: 'Novel',
-    },
-  ],
-};
-
-const store = createStore(rootReducer, state);
+import store from './app/store';
 
 ReactDOM.render(
   <React.StrictMode>
