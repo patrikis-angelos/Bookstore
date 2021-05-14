89cd7398c421688069818151522d97613f3f28cc
patrick-angelos <angelosp07@gmail.com>
2021-05-14 16:26:14 +0300

    Complete Book component styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -11,7 +11,7 @@ const Book = (props) => {
   return (
     <div className="book flex space-between align-center m-t-40 p-l-30">
       <div>
-        <p className="color-light-grey mont-bold standard m-b-10">{book.category}</p>
+        <p className="color-dark-grey mont-bold standard m-b-10">{book.category}</p>
         <p className="medium roboto-bold m-b-5">{book.title}</p>
         <p className="color-blue standard m-b-20 roboto-light">Author</p>
         <button type="button" className="book-button color-blue roboto-light m-r-10">Comment</button>
@@ -25,15 +25,18 @@ const Book = (props) => {
         <button type="button" className="book-button color-blue roboto-light m-r-10">Edit</button>
       </div>
       <div className="flex align-center">
-        <div className="complition m-r-20" />
-        <div className="text-center mont-regular color-grey">
+        <div className="complition-border m-r-20">
+          <div className="incomplete" />
+          <div className="complition-inner" />
+        </div>
+        <div className="text-center mont-regular color-black">
           <p className="big">0%</p>
-          <p className="color-light-grey standard mont-regular">Completed</p>
+          <p className="color-dark-grey standard mont-regular">Completed</p>
         </div>
       </div>
       <div>
-        <p className="standard roboto-light color-light-grey m-b-10">CURRENT CHAPTER</p>
-        <p className="regular roboto-light color-grey m-b-30">Chapter 1: &apos;Introduction&apos;</p>
+        <p className="standard roboto-light color-dark-grey m-b-10">CURRENT CHAPTER</p>
+        <p className="regular roboto-light color-black m-b-30">Chapter 1: &apos;Introduction&apos;</p>
         <button className="progress-button background-azure color-white roboto-light standard" type="button">UPDATE PROGRESS</button>
       </div>
     </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,7 +11,7 @@ const Nav = (props) => {
     <nav className="flex space-between nav align-center">
       <div className="flex align-center">
         <h1 className="mont-bold color-azure big">Bookstore CMS</h1>
-        <div className="mont-regular color-light-grey hover-grey standar m-l-40 pointer">BOOKS</div>
+        <div className="mont-regular color-light-grey hover-black standar m-l-40 pointer">BOOKS</div>
         <CategoryFilter filterHandler={handleFilterChange} />
       </div>
       <div className="icon-wrapper flex color-azure pointer"><i className="fas fa-user icon" /></div>
